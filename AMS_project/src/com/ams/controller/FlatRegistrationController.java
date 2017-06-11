faa21b439046c4a82cd58b7dee4d3ba6a74e94ae
riturajrathore <riturajrathore12@gmail.com>
2017-06-12 01:17:40 +0530

    test commit

--- AMS_project/src/com/ams/controller/FlatRegistrationController.java
+++ AMS_project/src/com/ams/controller/FlatRegistrationController.java
@@ -12,6 +12,7 @@ import com.ams.vo.FlatRegisterVO;
 /**
  * Servlet implementation class FlatRegistrationController
  */
+
 public class FlatRegistrationController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
